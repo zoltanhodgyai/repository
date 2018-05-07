98f6f1e3e3c78e7f1aa94c5a9add000f3375e836
Zoltan Hodgyai <zoltan.hodgyai@msg.group>
2018-05-07 12:04:48 +0300

    data.sql deleted. OrderDetail and Stock classes corrected

--- src/main/java/ro/msg/learning/shop/model/Stock.java
+++ src/main/java/ro/msg/learning/shop/model/Stock.java
@@ -1,16 +1,8 @@
 package ro.msg.learning.shop.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.Entity;
-
-@Entity
-@Getter
-@Setter
-@EqualsAndHashCode(of = "id")
+@Data
 public class Stock {
 
     private Integer product;
