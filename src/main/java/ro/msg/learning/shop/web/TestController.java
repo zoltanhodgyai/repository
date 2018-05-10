96055bffbd979c2a59f72b43c127b8e2ed671647
Zoltan Hodgyai <zoltan.hodgyai@msg.group>
2018-05-10 11:30:14 +0300

    TestController with two test html files for testing.

---
+++ src/main/java/ro/msg/learning/shop/web/TestController.java
@@ -0,0 +1,24 @@
+package ro.msg.learning.shop.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shop")
+public class TestController {
+
+    /*private OrderService orderService;
+
+    public TestController(OrderService orderService) {
+        this.orderService = orderService;
+    }*/
+
+    @GetMapping
+    public String index(Model model) {
+        //orderService.createOrder(new OrderDTO());
+
+        return "index";
+    }
+}
