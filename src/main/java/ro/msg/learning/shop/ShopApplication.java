116b2617406630a7291feaab65fab58bcd78a11e
Zoltan Hodgyai <zoltan.hodgyai@msg.group>
2018-05-15 18:10:33 +0300

    Exclude automatic security

--- src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -2,8 +2,9 @@ package ro.msg.learning.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ShopApplication {
 
 	public static void main(String[] args) {
